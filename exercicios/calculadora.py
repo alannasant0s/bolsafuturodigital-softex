6aad4e585d6fe4ebfcde8890b0f90cd0d44a88de
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-08-25 22:41:34 +0000

    Inserindo arquivos

---
+++ exercicios/calculadora.py
@@ -0,0 +1,2 @@
+numero1 = float(input("Insira o primeiro número: "))
+numero2 = float(input("Insira o segundo número: "))
---
+++ exercicios/calculo_de_media.py
@@ -0,0 +1,6 @@
+nota = float(input("Insira a nota do aluno(0 a 10):"))
+
+if nota >= 6.0:
+    print("Aluno Aprovado")
+else:
+    print("Aluno reprovado")
