3a9b38bb80828783f596e1261f0457e41c8dd9ef
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-08-25 23:25:03 +0000

    Inserindo arquivos

---
+++ exercicios/operadores.py
@@ -0,0 +1,10 @@
+numero = int (input("Insira um número de 1 a 3: "))
+
+if numero == 1:
+    print("Bom dia")
+elif numero == 2:
+    print("Boa tarde")
+elif numero == 3:
+    print("Boa noite")
+else:
+    print("Por favor, digite uma opção válida")
