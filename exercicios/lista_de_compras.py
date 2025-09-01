dac04f466399fbeb4bab6ae96464f8b73c09cb3a
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-09-01 15:49:59 +0000

    upload de exercicio

---
+++ exercicios/lista_de_compras.py
@@ -0,0 +1,15 @@
+print("""|LISTA DE COMPRAS|
+Você pode adicionar até 5 itens. """)
+minha_lista = []
+
+
+def lista_de_compras(lista):
+    while len(lista) < 5:
+        novo_item = input("Insira o ítem: ")
+        lista.append(novo_item)
+        print(f"O ítem {novo_item} foi inserido")
+
+    print("\nSua lista de compras está cheia!")
+lista_de_compras(minha_lista)
+print("\nItens na sua lista:")
+print(minha_lista)
