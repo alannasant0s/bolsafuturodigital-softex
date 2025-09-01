4640da554d47a0ea274fd5dd0c6fc098350fd9f0
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-09-01 23:01:36 +0000

    Upload de exercicio

---
+++ exercicios/jogo_do_bicho.py
@@ -0,0 +1,8 @@
+import random
+
+print("""Bem vindo ao Jogo do Bicho 🐊 🦓 🐎
+Aqui todos os seus sonhos são valorizados!""")
+
+lista_bicho = ["Avestruz 1", "Águia 2", "Burro 3", "Borboleta 4","Cachorro 5","Cabra 6","Carneiro 7","Camelo 8","Cobra 9","Coelho 10","Cavalo 11","Elefante 12","Galo 13","Gato 14","Jacaré 15","Leão 16","Macaco 17","Porco 18","Pavão 19","Peru 20","Touro 21","Tigre 22","Urso 23","Veado 24","Vaca 25"]
+bicho_sorteado = random.choice(lista_bicho)
+print(bicho_sorteado)
