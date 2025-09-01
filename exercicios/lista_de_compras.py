41399402be74aee7374ee49b77d7a08e62b9aae8
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-09-01 17:31:35 +0000

    Upload de exercicio

--- exercicios/lista_de_compras.py
+++ exercicios/lista_de_compras.py
@@ -13,3 +13,5 @@ def lista_de_compras(lista):
 lista_de_compras(minha_lista)
 print("\nItens na sua lista:")
 print(minha_lista)
+print(f"\nEsse é o meu 2º item da lista: {minha_lista[1]}")
+print(f"\nEsse é o meu 3º item da lista: {minha_lista[2]}")
