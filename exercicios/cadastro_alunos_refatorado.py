85f9ed1a134d08221cec1484e656089a186cf456
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-09-03 17:34:23 +0000

    upload de exercicio

---
+++ exercicios/cadastro_alunos_refatorado.py
@@ -0,0 +1,33 @@
+alunos = []
+
+# Criando a função do menu inicial
+def inicio_menu():
+    print("""================================
+|Bem vindo ao Cadastro de Alunos|
+================================""")
+
+# Criando a função que solicita dados
+def solicitar_dados():
+    nome = input("Insira seu nome: ")
+    email = input("Insira seu email: ")
+    aluno_dicionario = {
+    "nome": nome,
+    "email": email
+}    
+    alunos.append(aluno_dicionario)
+    print(f"O aluno {nome} foi cadastrado com sucesso")
+
+#Criando a função de continuar
+def funcao_continuar():
+    continuar = int(input("Deseja cadastrar outro aluno? Digite 1 para sim/ 2 para sair: "))
+    if continuar == 1:
+            return solicitar_dados()
+    elif continuar == 2:
+            print("Fim do programa")
+    else: 
+            print("Nenhuma ação realizada, insira uma entrada válida.")   
+
+
+inicio_menu()
+solicitar_dados()
+funcao_continuar()
