a5643dc39e695043a584f38f75a2abdd0a54f08f
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-09-03 11:41:05 +0000

    renomeando o arquivo

---
+++ exercicios/menu_banco.py
@@ -0,0 +1,15 @@
+usuario = input("Qual o seu nome? ")
+print(f"Seja bem vindo(a) {usuario}, selecione a operação que deseja realizar")
+
+
+def menu_banco():
+    print("""|==============================| 
+|     BANCO NOSSO DINHEIRO     |
+|==============================|                  
+|     1 - Ver Saldo            |
+|     2 - Realizar Depósito    |
+|     3 - Realizar Saque       |
+|     4 - Sair                 |
+|==============================|""")
+
+menu_banco()
