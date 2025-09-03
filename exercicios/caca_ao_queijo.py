79c63b63ebf41341b8f6ecc145f61569916cf0da
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-09-03 23:47:16 +0000

    upload de exercicio

--- exercicios/caca_ao_queijo.py
+++ exercicios/caca_ao_queijo.py
@@ -1,3 +1,37 @@
-print("""|=============================|
+import random
+
+contador = 0
+comodos_da_casa = ["Armário","Geladeira","Caixa de Sapato","Fogão","Guarda Roupa","Banheiro","Cola Rato","Ratoeira"]
+encontrou = False
+resposta = random.randint(1,8)
+
+while contador < 5:
+    palpite_usuario = int(input("""|=============================|
 |  JOGO CAÇA AO QUEIJO 🐁🧀   |
-|=============================|""")
+|=============================|
+ONDE O QUEIJO ESTÁ? Selecione a opção:
+[1] Armário
+[2] Geladeira
+[3] Caixa de Sapato
+[4] Fogão
+[5] Guarda Roupa
+[6] Banheiro
+[7] Cola Rato
+[8] Ratoeira        
+Você tem 5 chances...   
+DIGITE O NÚMERO CORRESPONDENTE: """))
+
+    if palpite_usuario == resposta:
+        print("PARABÉNS, O RATO ENCONTROU O QUEIJO.🧀")
+        encontrou = True
+        break
+    
+    else:   
+        print(f"Você errou, o queijo não está aqui. Tente novamente.")
+    contador +=1
+    
+if not encontrou:
+        print("Lamento, suas tentativas acabaram!")
+
+resposta_correta = comodos_da_casa[resposta - 1]
+print(f"O local onde o queijo estava é na(o) {resposta} ({resposta_correta})")
