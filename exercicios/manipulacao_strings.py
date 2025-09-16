88a444fd9449fbf4cd99a7e13c5ef693aa6b0ee1
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-09-16 13:44:07 +0000

    upload de exercicio

---
+++ exercicios/manipulacao_strings.py
@@ -0,0 +1,5 @@
+frase = input("Insira a sua frase: ")
+contagem = len(frase)
+print(f"Sua frase possui {contagem} letras")
+frase_invertida = frase[::-1]
+print(f"Sua frase invertida é: '{frase_invertida}'")
