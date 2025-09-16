d7c4860197a66a9dc1b9ee8250b2e53161b45709
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-09-16 08:22:35 -0300

    Delete exercicios_python directory

--- exercicios_python/exercicio_variaveis.py
+++
@@ -1,5 +0,0 @@
-nome = input("Qual o seu nome? ")
-idade = int(input("Qual a sua idade? "))
-altura = float(input("Insira a sua altura: "))
-
-print(f"Seja bem vindo(a) {nome}, sua altura é {altura} e sua idade é {idade}.Seus dados foram salvos com sucesso.")
