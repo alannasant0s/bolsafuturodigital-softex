f1504abae9ca862424b39dd4d72d0419acd98550
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-09-30 11:47:51 +0000

    refatorando

--- exercicios/match_case.py
+++ exercicios/match_case.py
@@ -3,12 +3,12 @@ while n != 0:
     n = int(input("Digite um número inteiro"))
     match n:
         case 1:
-            print("Você digitou um")
+            print("Você digitou um: ")
         case 2:
-            print("Você digitou dois")
+            print("Você digitou dois: ")
         case 3:
-            print("Você digitou três")
+            print("Você digitou três: ")
         case _:
-            print("Você digitou outra coisa")
+            print("Você digitou outra coisa.")
         
     print("Fim do programa")
