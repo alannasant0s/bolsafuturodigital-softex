3f57105d784928dd7abb7bd2a80027d3a5b3a85f
Alanna Santos <148654862+alannasant0s@users.noreply.github.com>
2025-10-06 13:46:22 +0000

    refatorando

--- exercicios/exercicio_dict.py
+++ exercicios/exercicio_dict.py
@@ -2,12 +2,12 @@ produtos = {}
 print("Leitura dos dados")	
 cod = input("Digite o código: ")
 while cod != "":
-    preco = float(input("Insira o valor do produto"))
+    preco = float(input("Insira o valor do produto: "))
     produtos[cod] = preco
     cod = input("Digite o código: ")
 
 print("Fim da leitura")
 print("Preço dos produtos")
 for cod in produtos.keys():
-    print(f"Produto {cod} custa R$ {preco}")
+    print(f"Produto {cod} custa R$ {produtos[cod]}")
 print("\nFim do programa")
